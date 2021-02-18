07235c7cb3be1181d6d3ce716fec76f808b3fb62
Jose Andres Hernandez <joseandreshernandezross@gmail.com>
2021-02-18 11:31:07 -0400

    App module fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CommingSoonComponent } from './comming-soon/comming-soon.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MainComponent } from './components/main/main.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './components/body/body.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommingSoonComponent,
-    MainComponent,
-    HeaderComponent,
-    BodyComponent
+    CommingSoonComponent
   ],
   imports: [
     BrowserModule,
