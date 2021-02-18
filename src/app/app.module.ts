8c1e2510f39aef8b5a0ad8077e98324746c3dd56
Jose Andres Hernandez <joseandreshernandezross@gmail.com>
2021-02-18 11:28:34 -0400

    Package.json modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CommingSoonComponent } from './comming-soon/comming-soon.component';
-import { ReactiveFormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommingSoonComponent
+    CommingSoonComponent,
+    MainComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
